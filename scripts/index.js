ef34fcd9a610fb6c9e27dfc7293a22cbb6533d10
Apozn <apoznaharev@gmail.com>
2024-05-15 01:28:06 +0300

    Create function removeCardItem and addCardItem

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,19 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
-// @todo: DOM узлы
+const cardList = document.querySelector(".places__list");
 
-// @todo: Функция создания карточки
+const removeCardItem = (cardItem) => {
+  cardItem.remove();
+};
 
-// @todo: Функция удаления карточки
+const addCardItem = ({ name, link }) => {
+  const cardItem = cardTemplate.querySelector(".places__item").cloneNode(true);
+  cardItem.querySelector(".card__image").src = link;
+  cardItem.querySelector(".card__title").textContent = name;
+  cardItem.querySelector(".card__delete-button").addEventListener("click", () => {
+    removeCardItem(cardItem);
+  });
+  cardList.append(cardItem);
+};
 
-// @todo: Вывести карточки на страницу
+initialCards.forEach(addCardItem);
